80d66a2f6f2a963668f6759d3ade6d08360e4e2d
KarimAgliullin <agliullinkarim1@gmail.com>
2024-11-19 15:16:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-name = 'Karim'
-print(name)
-age = 16
-print(age)
-new_age = 18
-print( age + new_age)
-is_student =  True
-print(is_student)
+Name = "Karim"
+print(Name)
+Age = 16
+print(Age)
+New_Age = Age + 1
+print(New_Age)
+is_student = True
+print( is_student)
